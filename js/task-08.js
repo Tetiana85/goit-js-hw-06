9f7ca632dd80dbb4447e403715867d4da7094d49
Tetiana85 <4215654@gmail.com>
2023-10-01 18:52:53 +0200

    task 7-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('#login-form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const { elements } = event.currentTarget;
+    const email = elements.email.value;
+    const password = elements.password.value;
+
+    if (email === '' || password === '') { 
+        return alert('Усі поля повинні бути заповнені!');
+    }
+
+    const userDetails = { email, password }; 
+
+    console.log(userDetails);
+    event.currentTarget.reset();
+}
 
